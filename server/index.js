3ab4bfbec9d802886e53556dd11e56942ba6f3f7
Harshit <harshitbisht1210@gmail.com>
2024-05-12 22:10:15 +0530

    try fix 6

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const notesRouter = require('./routes/notes.js');
-require('dotenv').config();
-require('./db/mongoose.js');
+const cors = require("cors");
+const notesRouter = require("./routes/notes.js");
+require("dotenv").config();
+require("./db/mongoose.js");
 const port = process.env.PORT || 4000;
 
-app.use(cors());
-app.use(express.json());
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "GET", "DELETE"],
+    allowedHeaders: ["Content-Type"],
   })
+);
+app.use(express.json());
 app.use(notesRouter);
 
-app.get('/', (req, res) => {
-    console.log("HELLO GUYS");
-    res.send({ data: "HELLO"});
+app.get("/", (req, res) => {
+  console.log("HELLO GUYS");
+  res.send({ data: "HELLO" });
 });
 
 app.listen(port, () => {
-    console.log(`Listening to http://localhost:${port}`);
+  console.log(`Listening to http://localhost:${port}`);
 });
