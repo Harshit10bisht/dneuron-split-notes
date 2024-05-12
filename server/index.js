dc029ed5c0edde10bbfaf622bd0acca4690eef4a
Harshit <harshitbisht1210@gmail.com>
2024-05-12 21:41:39 +0530

    try fix 3

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 require('./db/mongoose.js');
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use("/", notesRouter);
 
